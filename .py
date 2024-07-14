479c4db8b25e63b5a72fd1a744404a3a958b46ae
July11-stack <affan09shaik@gmail.com>
2024-07-14 00:07:40 -0400

    Add files via upload

--- .py
+++ .py
@@ -1,50 +1,12 @@
-print("Welcome to Affan's Assistant!!!")
-print("If you need help type 'help'")
-quit = False
-while not quit:
-    command = input(">")
-    if command.lower() == "quit":
-        quit = True
-        print("Program Ended")
-    elif command.lower() == "help":
-        print("""
-        help - find commands for affan's assistant
-        calculate - to use a calculator
-        smack - to smack ammaar and zayaan if you do this the program will end  at the end.
-        Secret Project - enter a password to enter this program!""")
-    elif command.lower() == "secret project":
-        password = input("Enter the Password:")
-        if password.lower() == "qwerty":
-            print("Ammaar is a good doggy!!")
-            quit = True
+class Person:
+    def name(self):
+        print('My name is Affan Shaik')
 
+    def talk(self):
+        print(message)
 
-        else:
-            print("Wrong Password TRY AGAIN!!!")
-    elif command.lower() == "smack":
-        quit = True
-        smack = input("Who Shall you hit Ammaar or Zayaan?")
-        if smack.lower() == "ammaar":
-            print("Ammaaar is a good doggy!")
-        if smack.lower() == "zayaan":
-            print("Zayaan is a good doggy!")
-    elif command.lower() == "calculate":
-        cal = input("Enter (a)dd,(s)ubtract,(m)ultiply, and (d)ivide: ")
-        if cal.lower() == "a":
-            var1 = int(input("Enter the first Variable:"))
-            var2 = int(input("Enter the second Variable:"))
-            print(f"The solution is {var1+var2}")
-        if cal.lower() == "s":
-            var1 = int(input("Enter the first Variable:"))
-            var2 = int(input("Enter the second Variable:"))
-            print(f"The solution is {var1-var2}")
-        if cal.lower() == "m":
-            var1 = int(input("Enter the first Variable:"))
-            var2 = int(input("Enter the second Variable:"))
-            print(f"The solution is {var1*var2}")
-        if cal.lower() == "d":
-            var1 = int(input("Enter the first Variable:"))
-            var2 = int(input("Enter the second Variable:"))
-            print(f"The solution is {var1/var2}")
-    else:
-         print("Type help to get a list of commands!!!")
+person = Person()
+
+person.name()
+message = input("Enter a statement:")
+person.talk()
